54e468376ee34756035a8c06ca5922e8f3fa866e
Dgley Ramos <dgleysilva@gmail.com>
2022-02-21 14:46:15 -0300

    Adicionando função disparo

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,7 @@ function start(){
 
     var velocidade=5;
     var posicaoY = parseInt(Math.random() * 334);
+    var podeAtirar=true;
 
     //Verifica se o usuário pressionou alguma tecla	
 	
@@ -85,7 +86,8 @@ function start(){
         
         if (jogo.pressionou[TECLA.D]) {
             
-            //Chama função Disparo	
+            //Chama função Disparo
+            disparo();	
         }
     
     } // fim da função movejogador()
@@ -126,6 +128,39 @@ function start(){
             }
     
     } // fim da função moveamigo()
+
+    function disparo() {
+	
+        if (podeAtirar==true) {
+            
+            podeAtirar=false;
+            
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX= parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro=topo+37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top",topoTiro);
+            $("#disparo").css("left",tiroX);
+            
+            var tempoDisparo=window.setInterval(executaDisparo, 30);
+        
+        } //Fecha podeAtirar
+     
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left",posicaoX+15); 
+
+            if (posicaoX>900) {
+                        
+                window.clearInterval(tempoDisparo);
+                tempoDisparo=null;
+                $("#disparo").remove();
+                podeAtirar=true;
+                    
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
     
 
 }
