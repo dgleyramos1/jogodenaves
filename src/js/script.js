f2d7295b8c5b2036e58948af8cb626d5bb1e6583
Dgley Ramos <dgleysilva@gmail.com>
2022-02-21 14:28:04 -0300

    adicionando condicionais para manter o helicóptero na div fundogame

--- src/js/script.js
+++ src/js/script.js
@@ -56,14 +56,25 @@ function start(){
 	
         if (jogo.pressionou[TECLA.W]) {
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo-10);
+            
+            if (topo<=0) {
+                $("#jogador").css("top",topo+10);
+            }else{
+                $("#jogador").css("top",topo-10);
+            }
+            
         
         }
         
         if (jogo.pressionou[TECLA.S]) {
             
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo+10);	
+            
+            if (topo>=434) {	
+                $("#jogador").css("top",topo-10);   
+            }else{
+                $("#jogador").css("top",topo+10);
+            }
         }
         
         if (jogo.pressionou[TECLA.D]) {
