0503557f81ffef3fd687eeba55f3f7899edfa4f5
Dgley Ramos <dgleysilva@gmail.com>
2022-02-21 13:39:50 -0300

    Adicionando e removendo divs

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+
+//Inicio da função start()
+function start(){
+    $("#inicio").hide();
+
+    $("#fundoGame").append("<div id='jogador'></div>");
+    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='inimigo2'></div>");
+    $("#fundoGame").append("<div id='amigo'></div>");
+}
+// Fim da função start()
